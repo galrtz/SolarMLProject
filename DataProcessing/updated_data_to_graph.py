d7e73be265d5db9e3cea499b84536b9ee35122ff
galrtz <galrtz@gmail.com>
2025-01-09 13:49:04 +0200

    new update

--- DataProcessing/updated_data_to_graph.py
+++ DataProcessing/updated_data_to_graph.py
@@ -5,7 +5,7 @@ from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 import os
 import pickle
-import time
+import time11
 33333333333
 
 # Save the DataLoader's data_list to a .pkl file
