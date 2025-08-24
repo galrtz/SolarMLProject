3d197d02535e1b629e05304d213abf973e4fd359
hadarl1 <hadar7937@gmail.com>
2025-08-24 23:15:12 +0300

    Update splitting.py

--- data_proccessing/splitting.py
+++ data_proccessing/splitting.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 from tqdm import tqdm
 
-file_path = r"C:\Users\hadar\Desktop\אוניברסיטה\פרויקט גמר\relevant_directories\testing\data_2018_train.csv"
+file_path = r"C:\Users\<user>\Desktop\relevant_directories\testing\data_2018_train.csv"
 
 
 df = pd.read_csv(file_path)
@@ -17,8 +17,7 @@ df = pd.read_csv(file_path)
 # df["Day"] = df["datetime"].dt.day
 # df["Hour"] = df["datetime"].dt.hour
 
-output_dir = r"C:\Users\hadar\Desktop\אוניברסיטה\פרויקט גמר\relevant_directories\testing\data_2018_train_splitting"
-# output_dir = r"C:\Users\hadar\Desktop\אוניברסיטה\פרויקט גמר\relevant_directories\testing\data_2017_test_splitting"
+output_dir = r"C:\Users\<user>\Desktop\relevant_directories\testing\data_2018_train_splitting"
 
 
 os.makedirs(output_dir, exist_ok=True)
@@ -26,9 +25,7 @@ grouped = df.groupby(["datetime"])
 
 
 for daytime, group in tqdm(grouped):
-    # print(daytime)
     print(daytime)
-    # print(group)
     dt = datetime.strptime(daytime[0], "%Y-%m-%d %H:%M:%S")  # dayfirst
 
     year = dt.year
@@ -42,4 +39,4 @@ for daytime, group in tqdm(grouped):
     full_path = os.path.join(output_dir, filename)
     group.to_csv(full_path, index=False)
 
-print(f"✅ Created {len(os.listdir(output_dir))} CSV files in {output_dir}")
+print(f"Created {len(os.listdir(output_dir))} CSV files in {output_dir}")
